9695a86a08fa0962c5c85bac3941431fc2f83d1d
Sujan STC <sujandstc@gmail.com>
2023-03-19 13:18:00 +0545

    Changes

--- src/serv.js
+++ src/serv.js
@@ -30,7 +30,7 @@ export function registerServiceWorker() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("/serv.js")
+        .register("./serv.js")
         .then((reg) => {
           console.log("Service worker registered!", reg);
         })
